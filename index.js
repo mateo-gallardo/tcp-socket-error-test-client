62588980bdad33d96a8418aaaf87fecab8ce75f7
Mateo Gallardo <mateo@noblypos.com>
2020-06-17 15:27:17 -0300

    Client that sends a JSON through a TCP socket every second

---
+++ index.js
@@ -0,0 +1,26 @@
+const net = require('net');
+
+const IP = '192.168.1.9';
+const PORT = 12345;
+
+const objectToSend = { someProp: 1, anotherProp: '2' };
+const json = JSON.stringify(objectToSend);
+let jsonsSent = 0;
+
+const sendJSON = () => {
+  setTimeout(() => {
+    const client = new net.Socket();
+    client.connect(PORT, IP, () => {
+      console.log('Connected');
+      client.write(json);
+      client.destroy();
+    });
+    
+    client.on('close', () => {
+      jsonsSent++;
+      console.log(`Connection closed. Sent ${jsonsSent} JSONs so far`);
+      sendJSON();
+    });
+  }, 1000);
+};
+sendJSON();
